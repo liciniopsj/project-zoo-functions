7fb8c6b08961a97275a24a1ef3ae5e1315f2e5a1
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-09-29 17:05:02 -0300

    Requisite 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const findAnimal = (animal) => species.find((specie) => specie.name === animal);
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const match = findAnimal(animal);
+  const { residents } = match;
+  return residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
