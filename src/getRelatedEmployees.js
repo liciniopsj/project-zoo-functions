f46d745f5a1b6d24d8f5f5020969c4b0704b89c0
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-09-29 19:14:14 -0300

    Requisite 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const managers = [];
+employees.forEach((employee) => {
+  managers.push(employee.managers);
+});
+const mergedManagersValues = [].concat(...managers);
+const nonDuplicateManagerArray = [...new Set(mergedManagersValues)];
+
+const findRelated = (id) => {
+  const returnArray = [];
+  employees.forEach((employee) => {
+    if (employee.managers.some((managerId) => managerId === id)) {
+      returnArray.push(`${employee.firstName} ${employee.lastName}`);
+    }
+  });
+  return returnArray;
+};
+
 function isManager(id) {
-  // seu código aqui
+  return nonDuplicateManagerArray.some((manager) => manager === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return findRelated(managerId);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
