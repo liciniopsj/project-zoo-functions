0eaee3fa72d22c41989e345f001c6d6824229756
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-09-29 17:31:21 -0300

    Requisite 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+const findEmployeeByName = (employeeName) => {
+  const match = employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  return match;
+};
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const employee = {};
+  Object.assign(employee, findEmployeeByName(employeeName));
+  return employee;
 }
+
+console.log(typeof findEmployeeByName('Nelson'));
 
 module.exports = getEmployeeByName;
